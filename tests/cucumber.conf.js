23d724e6a6584be911378d5be3de134a894a482a
prashant-gurung899 <prasantgrg777@gmail.com>
2024-03-27 15:16:15 +0545

    Changed Port number

--- tests/cucumber.conf.js
+++ tests/cucumber.conf.js
@@ -12,7 +12,7 @@ setDefaultTimeout(60000);
 // launch the browser
 BeforeAll(async function () {
   global.browser = await chromium.launch({
-    headless: false,
+    headless: true,
     slowMo: 5000,
   });
 });
