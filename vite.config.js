d4d4327a605f4c7221f8f82122e72a30a88fbb70
ishabaral <baralisha.16@gmail.com>
2024-03-28 09:41:42 +0545

    changes from master branch

--- vite.config.js
+++ vite.config.js
@@ -6,13 +6,13 @@ export default defineConfig({
   base: "/",
   plugins: [react()],
   preview: {
-    port: 8081,
+    port: 8080,
     strictPort: true
   },
   server: {
-    port: 8081,
+    port: 8080,
     strictPort: true,
     host: true,
-    origin: "http://0.0.0.0:8081"
+    origin: "http://0.0.0.0:8080"
   }
 });
