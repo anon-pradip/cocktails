194641e9751a23ca8fd5e79371874ec38105468e
ishabaral <baralisha.16@gmail.com>
2024-04-10 12:23:03 +0545

    used env variables

--- tests/e2e/pageObjects/homePage.js
+++ tests/e2e/pageObjects/homePage.js
@@ -1,13 +1,15 @@
+require('dotenv').config();
+
 class HomePage {
   constructor() {
-    this.url = "http://localhost:8080/";
+    this.url = process.env.URL? process.env.URL : "http://localhost:8080/";
     this.homeSelector = '//nav/div[1]/a[@href="/"]';
   }
 
   navigateToHomePage = async () => {
     await page.goto(this.url);
     const locate = await page.locator(this.homeSelector);
-    return locate
+    return locate;
   };
 }
 
